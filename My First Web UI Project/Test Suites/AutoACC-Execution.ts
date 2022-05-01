702cb5f4b2051d0edd5d28a637d885aa4908b81f
kenliu1213 <40820762+kenliu1213@users.noreply.github.com>
2022-05-01 18:22:54 +0800

    Add AutoACC Rep
    Add AutoACC Rep

---
+++ My First Web UI Project/Test Suites/AutoACC-Execution.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AutoACC-Execution</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f1baf4b8-1ef0-4847-93d0-8c6085e8d1a5</testSuiteGuid>
+   <testCaseLink>
+      <guid>ae495682-d18d-45f2-880d-e9911a0f21e6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AutoACC/AutoACC_login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
